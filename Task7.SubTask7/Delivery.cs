fe5ea5f8ac92fc9ee4eb40c15ca5bf752366a47f
Philipp Kucherovskiy <philipp.kucherovskiy@gmail.com>
2023-01-14 22:26:00 +0300

    Последний результат

---
+++ Task7.SubTask7/Delivery.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+
+abstract class Delivery
+{
+    public string Address;
+    public string[] UserData;
+    public virtual void DisplayAddress()
+    {
+        Console.WriteLine(Delivery.Address);
+    }
+}
+    
+
+
+
+
--- Task7.SubTask7/Program.cs
+++ Task7.SubTask7/Program.cs
@@ -1,22 +1,23 @@
 ﻿
 using System;
-
-abstract class Delivery
-{
-    public string Address;
-    public virtual void DisplayAddress()
-    {
-        Console.WriteLine(Delivery.Address);
-    }
-}
 public class Warehouse 
 {
     public string Name { get; set; }
 
     public string Comment { get; set; }
 
-    public int GoodsId { get; set; }
+    public string GoodsId { get; set; }
 }
+public class Cart
+{
+    private Warehouse warehouse;
+
+    public string SystemUnit(Warehouse warehouse)
+    {
+        this.warehouse=warehouse;  // агрегация от склада
+    }
+}
+
 class Order<TDelivery,
 TStruct> where TDelivery : Delivery
 {
@@ -24,43 +25,98 @@ TStruct> where TDelivery : Delivery
 
     public int Number;
 
-    public string Description;
+    public string Description; }
+
 
     public class ShipmentItem : Warehouse
-{
-    public int ShipmentId { get; set; }
-
-    public int OrderItemId { get; set; }
-
-    public int Quantity { get; set; }
-
-    public int WarehouseId { get; set; }
-}
- class Shipment : Delivery
-{
-    public int OrderId { get; set; }
-    public string TrackingNumber { get; set; }
-    public decimal? TotalWeight { get; set; }
-    public string Comment { get; set; }
-    public class ShippingMethod : Delivery
     {
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public int DisplayOrder { get; set; }
+        public int ShipmentId { get; set; }
+
+        public int OrderItemId { get; set; }
+
+        public int Quantity { get; set; }
+
+        public int WarehouseId { get; set; }
+
     }
-    public class ShipmentCreatedEvent
+    class ShipmentCreatedEvent : Delivery                                                              //наследование
     {
-        public ShipmentCreatedEvent(Shipment shipment)
+        public int OrderId { get; set; }
+        public string TrackingNumber { get; set; }
+        public decimal? TotalWeight { get; set; }
+        public string Comment { get; set; }
+        public class ShippingMethod : Delivery
         {
-            Shipment = shipment;
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public int DisplayOrder { get; set; }
+        }
+    class ShipmentCreatedEvent
+    {
+        string s = "Создана новая доставка"
+            char c = 'и';
+        private readonly int i = s.CharCount(c);
+
+        public ShipmentCreatedEvent(int i)
+        {
+            this.i = i;
         }
 
-        public Shipment Shipment { get; }
-    }
-    public class PickupPoint
-    {
+        Console.WriteLine(i); }
 
-        public string Id { get; set; }
+
+
+    public ShipmentCreatedEvent(string s, char c, int i, global::ShipmentCreatedEvent shipment) // конструктор с проверкой значений
+            {
+                this.s = s ?? throw new ArgumentNullException(nameof(s));
+                this.c = c;
+                this.i = i;
+                Shipment = shipment ?? throw new ArgumentNullException(nameof(shipment));
+            }
+
+            public void ShipmentCreatedEvent() => Console.WriteLine("Создание объекта ShipmentCreatedEvent"); // конструктор с 1 методом
+
+            ShipmentCreatedEvent = new ShipmentCreatedEvent();   //вызов конструктора 
+
+            public ShipmentCreatedEvent(global::ShipmentCreatedEvent shipment)
+            {
+                Shipment = shipment;
+            }
+
+            public global::ShipmentCreatedEvent Shipment { get; }
+        }    
+    } 
+}
+public static class ShipmentExtension
+{
+    public static int CharCount(this string str, char c)
+    {
+        int counter = 0;
+        for (int i = 0; i < str.Length; i++)
+        {
+            if (str[i] == c)
+                counter++;
+        }
+        return counter;
+    }
+
+    public class PickupPoint
+
+    {
+        public static PickupPoint operator +(PickupPoint counter1, PickupPoint counter2) // перегрузка оператора
+        {
+            return new PickupPoint { Value = counter1.Value + counter2.Value };
+        }
+        public static bool operator >(PickupPoint counter1, PickupPoint counter2)
+        {
+            return counter1.Value > counter2.Value;
+        }
+        public static bool operator <(PickupPoint counter1, PickupPoint counter2)
+        {
+            return counter1.Value < counter2.Value;
+        }
+
+        public string Id { get; set; }//свойства
 
 
         public string Name { get; set; }
@@ -77,25 +133,26 @@ TStruct> where TDelivery : Delivery
 
         public string ZipPostalCode { get; set; }
 
-        //координаты
+        //координаты 
         public decimal? Latitude { get; set; }
 
         public decimal? Longitude { get; set; }
 
         // дни в пути
         public int? TransitDays { get; set; }
+        public int Value { get; private set; }
     }
 
     public class DeliveryDate : Delivery
     {
         public string Name { get; set; }
-            public object Delivery { get; private set; }
+        public object Delivery { get; private set; }
 
-            public override void DisplayAddress()         //переопределение метода
-    {
-        Console.WriteLine(Delivery.Address);
-                
-    }
+        public override void DisplayAddress()         //переопределение метода
+        {
+            Console.WriteLine(Delivery.Address);
+
+        }
     }
     public enum ShippingStatus
     {
@@ -118,7 +175,7 @@ TStruct> where TDelivery : Delivery
     }
     public class ShippingOption
     {
-        
+
         public string ShippingRateMethodName { get; set; }
 
         public decimal Rate { get; set; }
@@ -135,13 +192,14 @@ TStruct> where TDelivery : Delivery
     }
     public class ShipmentDeliveredEvent
     {
-        public ShipmentDeliveredEvent(Shipment shipment)
+        public ShipmentDeliveredEvent(ShipmentCreatedEvent shipment)
         {
             Shipment = shipment;
         }
-        public Shipment Shipment { get; }
-    }
+        public ShipmentCreatedEvent Shipment { get; }
+    } 
 }
+    
 
 
 
