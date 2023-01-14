b49c33d625bd9557ba1d027ed8b976a1818dc8c5
Philipp Kucherovskiy <philipp.kucherovskiy@gmail.com>
2023-01-14 17:00:03 +0300

    1 версия

--- Task7.SubTask7/Program.cs
+++ Task7.SubTask7/Program.cs
@@ -4,9 +4,71 @@ namespace Task7.SubTask7
 {
     class Program
     {
-        static void Main(string[] args)
+        abstract class Delivery
         {
-            Console.WriteLine("Hello World!");
+            public string Name;
+            public string SurName;
+            public string Address;
+            public int DeliveryType;
+        }
+
+        class HomeDelivery : Delivery
+        { }
+
+}
+
+        class PickPointDelivery : Delivery
+        {
+            /* ... */
+        }
+
+        class ShopDelivery : Delivery
+        {
+            /* ... */
+        }
+        //Класс товаров, имеющихся в наличии и доступных к продаже.
+        //Равнозначен с классом товаров на складе, указывает на него ассоциацией
+        class Itemsforsale
+        {
+            private Order order;
+
+        }
+    //Корзина покупок связана композицией с классом товаров, наличествующих к продаже.
+    //Выбрана композиция,а не агрегация,  т.к. в моем представлении экземпляр класса корзины не должен существовать вне имеющихся товаров.
+
+    protected class Cart
+    {
+            private Itemsforsale itemsforsale;
+            public Cart()
+            {
+                itemsforsale = new Itemsforsale();
+            }
+        }
+        {
+
+        }
+
+    internal class Order
+    {
+    }
+} }
+        
+    
+    class Order<TDelivery,
+        TStruct> where TDelivery : Delivery
+        {
+            public TDelivery Delivery;
+
+            public int Number;
+
+            public string Description;
+
+            public void DisplayAddress()
+            {
+                Console.WriteLine(Delivery.Address);
+            }
+
+            
         }
     }
 }
